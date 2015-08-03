bfb8cd2f25b5b4c65f6d142ea74df76024c75cc5
keigonec <keigonec@gmail.com>
2015-08-03 19:17:18 +0800

    XiiUploader v0.2
    有点时间，就把缩略图做了。。。

--- XiiUploader.php
+++ XiiUploader.php
@@ -11,13 +11,26 @@
  * 功能: 基于Yii2的文件上传类
  * 说明: 对Yii2的UploadedFile以及Xii2的XiiFolder有依赖
  *
- * 版本: Ver0.1 Build 20150803
+ * What's new ?
+ * Ver0.3 Build 2015????
+ * -  预告：计划使用七牛存储，增加相关函数，另因七牛可以在输出时增加水印，所以就暂不额外开发了
+ *
+ * Ver0.2 Build 20150803
+ * -  增加缩略图上传功能，缩略图保存路径和原图完全一致，只是缩略图增加了_thumb后缀（可自定义）
+ * -  例如：原图：uploads/20150803/abcdef.jpg；缩略图：uploads/20150803/abcdef_thumb.jpg
+ *
+ * Ver0.1 Build 20150803
  * -  实现文件单个或多个（数组形式）上传；自定义目录；年月日8位日期目录；文件大小过滤；类型过滤；文件名sha256处理
  * 
  * 示例: 
  *      需要app\xii\XiiFolder;
  *      use app\xii;
  *      use app\xii\XiiUploader;
+ *
+ *      //XiiUploader::$_ThumbnailNeed = false;
+ *      XiiUploader::$_ThumbnailPercent = 0.1;
+ *      //XiiUploader::$_ThumbnailWidth = 100;
+ *      //XiiUploader::$_ThumbnailHeight = 100;
  *      $test = XiiUploader::run('file');
  *      var_dump($test);
  *      失败范例：
@@ -32,8 +45,12 @@
  *        'status' => boolean true
  *        'file' => 
  *          array (size=2)
- *              0 => string 'uploads/20150803/c9e0fe26af017c3fae71f0e37c52c8c6a9ed623d13f0bcf7384279639fb7a6f3.jpg' (length=85)
- *              1 => string 'uploads/20150803/f793a3f09a6fc6f94554c0c0c92b51d7db6ab2a2e99a079698cbe7de433b48d1.jpg' (length=85)
+ *            0 => string 'uploads/20150803/9897d4d618dee40468125f93264db10ed43a25ca1df8f69f23bc6a6aed3a9518.jpg' (length=85)
+ *            1 => string 'uploads/20150803/61488e85357daee8378c2a8ac77cc5bcb39dd7fcce9234650628a7e815d330ac.png' (length=85)
+ *        'thumb' => 
+ *          array (size=2)
+ *            0 => string 'uploads/20150803/9897d4d618dee40468125f93264db10ed43a25ca1df8f69f23bc6a6aed3a9518_thumb.jpg' (length=91)
+ *            1 => string 'uploads/20150803/61488e85357daee8378c2a8ac77cc5bcb39dd7fcce9234650628a7e815d330ac_thumb.png' (length=91)
  */
 
 namespace app\xii;
@@ -44,18 +61,36 @@ use app\xii\XiiFolder;
 
 class XiiUploader
 {
-    public static $_PathFolder = 'uploads';
-    public static $_PathUseDateFormat = true;
-    private static $_PathDateFormatStatus = false;
+    public static $_PathFolder = 'uploads'; //文件上传保存目录，可以带/，也可以不带
+    public static $_PathUseDateFormat = true; //是否在保存目录中自动建立20150808这样的日期目录
+    private static $_PathDateFormatStatus = false; //用于判断日期目录是否已经建立
 
-    public static $_SizeLimit = true;
-    public static $_SizeMin = '5k';
-    public static $_SizeMax = '300k';
-    private static $_SizeUnit = ['kb' => 1000, 'mb' => 1000000];
+    public static $_SizeLimit = true; //是否对文件大小进行限制
+    public static $_SizeMin = '5k'; //最小文件
+    public static $_SizeMax = '300k'; //最大文件
+    private static $_SizeUnit = ['kb' => 1000, 'mb' => 1000000]; //根据单位换算大小
 
-    public static $_FileTypeLimit = true;
-    public static $_FileTypeAllow = ['png', 'jpg', 'jpeg'];
-    public static $_FileNameEncrypt = true;
+    public static $_FileTypeLimit = true;  //是否对文件类型进行限制
+    public static $_FileTypeAllow = ['png', 'jpg', 'jpeg', 'gif']; //允许的文件类型后缀名
+    public static $_FileNameEncrypt = true; //是否使用sha256修改上传文件的名字
+
+    public static $_ThumbnailNeed = true; //是否生成缩略图
+    public static $_ThumbnailNeedOff = 'Thumbnail is Off!'; //缩略图关闭的提示语
+    public static $_ThumbnailSameType =true; //缩略图是否与原图同类型
+    public static $_ThumbnailPercent = 5; //是否按照比例缩小原图，设置范围 1 > x >= 0
+    public static $_ThumbnailWidth = 0; //缩略图宽度设置
+    public static $_ThumbnailHeight = 0; //缩略图高度设置
+    public static $_ThumbnailSuffix = '_thumb'; //缩略图文件名后缀，同目录保存，增加后缀
+
+    /*
+    缩略图，缩小尺寸优先级说明：
+    $_ThumbnailPercent > 0; 按照设置比例确定宽度和高度生成缩略图
+    $_ThumbnailPercent = 0; 按照缩略图宽度和高度生成缩略图
+    宽度和高度都不为0时，按照设置宽度高度生成缩略图
+    宽度不为0，高度为0，以宽度缩小比例设置高度，生成缩略图
+    高度不为0，宽度为0，以高度缩小比例设置高度，生成缩略图
+    三个设置都为0，则等同于不生成缩略图
+    */
 
     public static function run($para)
     {
@@ -66,6 +101,7 @@ class XiiUploader
             self::PreparePath();
 
             $feedback = [];
+            $thumbs = self::$_ThumbnailNeed ? [] : self::$_ThumbnailNeedOff;
 
             foreach($attaches['file'] as $v)
             {
@@ -73,6 +109,11 @@ class XiiUploader
                     
                 if($v->saveAs($tmp_name))
                 {
+                    if(self::$_ThumbnailNeed)
+                    {
+                        $thumb = self::CreateThumbnail($tmp_name);
+                        $thumbs[] = $thumb['status'] ? $thumb['file'] : $thumb['msg'];
+                    }
                     $feedback[] = $tmp_name;
                 }
             }
@@ -83,11 +124,11 @@ class XiiUploader
             {
                 if($files_num == 1)
                 {
-                    return ['status' => true, 'file' => reset($feedback)];
+                    return ['status' => true, 'file' => reset($feedback) , 'thumb' => $thumbs];
                 }
                 else
                 {
-                    return ['status' => true, 'file' => $feedback];
+                    return ['status' => true, 'file' => $feedback , 'thumb' => $thumbs];
                 }
                 
             }
@@ -101,6 +142,12 @@ class XiiUploader
         {
             return ['status' => false, 'msg' => $attaches['msg']];
         }
+    }
+
+    public static function check($para)
+    {
+        //name, size , extension
+        return self::PrepareCheck($para);
     }
 
     private static function PrepareUpload($para)
@@ -224,6 +271,107 @@ class XiiUploader
         }
 
         return ['status' => true, 'msg' => $para->name . ' is ready to upload!'];
+    }
+
+    private static function CreateThumbnail($para)
+    {
+        if (!function_exists("imagecopyresampled")) 
+        {
+            return ['status' => false, 'msg' => 'GD is not work well!'];
+        }
+
+        $file_info = @getimagesize($para);
+        $file_name = str_replace(self::$_PathFolder, '', $para);
+
+        if(!$file_info)
+        {
+            return ['status' => false, 'msg' => $file_name . ' is failed to get info!'];
+        }
+        else
+        {
+            list($width, $height, $type, $attr) = $file_info;
+        }
+
+        if($type > 3)
+        {
+            return ['status' => false, 'msg' => $file_name . ' is not valid type!'];
+        }
+
+        if((self::$_ThumbnailPercent > 0) && (self::$_ThumbnailPercent < 1))
+        {
+            $thumb_width = (int)($width * self::$_ThumbnailPercent);
+            $thumb_height = (int)($height * self::$_ThumbnailPercent);
+        }
+        else
+        {
+            if((self::$_ThumbnailWidth == 0) && (self::$_ThumbnailHeight == 0))
+            {
+                return ['status' => false, 'msg' => $file_name . ' is failed to create thumbnail without configuration!'];
+            }
+
+            if(self::$_ThumbnailWidth >= $width)
+            {
+                return ['status' => false, 'msg' => $file_name . ' is failed to create thumbnail with wrong width!'];
+            }
+
+            if(self::$_ThumbnailHeight >= $height)
+            {
+                return ['status' => false, 'msg' => $file_name . ' is failed to create thumbnail with wrong height!'];
+            }
+
+            $thumb_width = self::$_ThumbnailWidth;
+            $thumb_height = self::$_ThumbnailHeight;
+
+            if ($thumb_width == 0)
+            {
+                $thumb_width = (int)($width * $thumb_height / $height); 
+            }
+
+            if ($thumb_height == 0)
+            {
+                $thumb_height = (int)($height * $thumb_width / $width); 
+            }
+        }
+
+        switch ($type) 
+        {
+            //1 = GIF，2 = JPG，3 = PNG
+            case 1:
+                $source_img = imagecreatefromgif($para);
+                break;
+            
+            case 2:
+                $source_img = imagecreatefromjpeg($para);
+                break;
+
+            case 3:
+                $source_img = imagecreatefrompng($para);
+                break;
+        }
+
+        $thumb_img = imagecreatetruecolor($thumb_width, $thumb_height);
+        imagecopyresampled($thumb_img, $source_img, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+        
+        $thumb_file = str_replace('.', self::$_ThumbnailSuffix . '.', $para);
+
+        switch ($type) 
+        {
+            //1 = GIF，2 = JPG，3 = PNG
+            case 1:
+                $source_img = imagegif($thumb_img ,$thumb_file);
+                break;
+            
+            case 2:
+                $source_img = imagejpeg($thumb_img ,$thumb_file);
+                break;
+
+            case 3:
+                $source_img = imagepng($thumb_img ,$thumb_file);
+                break;
+        }
+        imagedestroy($thumb_img);
+
+        return ['status' => true, 'file' => $thumb_file];
     }
 }
 ?>
